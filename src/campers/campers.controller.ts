d4c5f4e66d3accafb38b688d48a5e3204d9ead3d
Gilmar Oliveira <giilmaroliveira@outlook.com>
2023-03-19 00:43:13 -0300

    feat(camper): implementing getAll camper query and handler

--- src/campers/campers.controller.ts
+++ src/campers/campers.controller.ts
@@ -1,19 +1,23 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CamperDto } from './camper.dto';
 import { CreateCamperCommand } from './commands/create-camper/create-camper.command';
 import { UpdateAllergiesCommand } from './commands/update-allergies/update-allergies.command';
 import { CreateCamperRequest } from './dto/request/create-camper-request.dto';
 import { UpdateCamperAllergiesRequest } from './dto/request/update-camper-allergies-request.dto';
+import { CampersQuery } from './queries/campers.query';
 
 @Controller('campers')
 export class CampersController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(private readonly commandBus: CommandBus, private readonly queryBus: QueryBus) {}
 
   @Get(':id')
   async getCamper(@Param('id') camperId: string): Promise<void> {}
 
   @Get()
-  async getCampers(): Promise<void> {}
+  async getCampers(): Promise<CamperDto[]> {
+    return this.queryBus.execute<CampersQuery, CamperDto[]>(new CampersQuery());
+  }
 
   @Post()
   async createCamper(@Body() createCamperRequest: CreateCamperRequest): Promise<void> {
