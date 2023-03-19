4894ae4f61855ba33ae7fea2b440a6863bc77a79
Gilmar Oliveira <giilmaroliveira@outlook.com>
2023-03-18 23:58:58 -0300

    feat(campers): implement create method in controller

--- src/campers/campers.controller.ts
+++ src/campers/campers.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateCamperCommand } from './commands/create-camper.command';
 import { CreateCamperRequest } from './dto/request/create-camper-request.dto';
 import { UpdateCamperAllergiesRequest } from './dto/request/update-camper-allergies-request.dto';
 
 @Controller('campers')
 export class CampersController {
-  constructor() {}
+  constructor(private readonly commandBus: CommandBus) {}
 
   @Get(':id')
   async getCamper(@Param('id') camperId: string): Promise<void> {}
@@ -13,9 +15,9 @@ export class CampersController {
   async getCampers(): Promise<void> {}
 
   @Post()
-  async createCamper(
-    @Body() createCamperRequest: CreateCamperRequest,
-  ): Promise<void> {}
+  async createCamper(@Body() createCamperRequest: CreateCamperRequest): Promise<void> {
+    await this.commandBus.execute<CreateCamperCommand, void>(new CreateCamperCommand(createCamperRequest));
+  }
 
   @Patch(':id')
   async updateCamperAllergies(
